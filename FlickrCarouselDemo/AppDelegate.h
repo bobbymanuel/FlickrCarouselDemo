fe9dc1b6c8042f0332c57cd746702ef7781cbd70
Bobby Manuel <bobby.manuel@gmail.com>
2015-01-29 21:09:28 -0800

    Initial Commit

---
+++ FlickrCarouselDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrCarouselDemo
+//
+//  Created by Bobby Manuel on 1/29/15.
+//  Copyright (c) 2015 Bobby Manuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrCarouselDemo/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  FlickrCarouselDemo
+//
+//  Created by Bobby Manuel on 1/29/15.
+//  Copyright (c) 2015 Bobby Manuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
